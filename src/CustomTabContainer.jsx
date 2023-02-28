5eaac6ab885e1a1e83407f457ae21f7bed284265
Muhammad Asad <muhammada@aiologic.io>
2023-02-28 06:18:59 -0800

    Fixed the issue with persistence

--- src/CustomTabContainer.jsx
+++ src/CustomTabContainer.jsx
@@ -1,7 +1,8 @@
 import { Big } from "big.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const CustomTabContainer = ({ targetTabCtrl, currentUserId, currentEntityId, entityName }) => {
+    const [lastPath, setLastPath] = useState(window.location.pathname);
     const checkTargetDivPresent = () => {
         const divList = document.getElementsByClassName("mx-name-" + targetTabCtrl);
         if (divList.length === 0) {
@@ -10,16 +11,19 @@ const CustomTabContainer = ({ targetTabCtrl, currentUserId, currentEntityId, ent
     };
 
     useEffect(() => {
+        setLastPath(window.location.pathname);
+    }, [])
+
+    useEffect(() => {
         if (currentUserId?.items?.[0]?.id && currentEntityId?.items?.[0]?.id) {
             checkTargetDivPresent();
             if (document.querySelectorAll(".mx-name-" + targetTabCtrl + " > ul > li").length === 0) {
                 console.error("Unable find tab pages");
                 return;
             }
-            console.info(`${currentUserId?.items?.[0]?.id}-${entityName}-${currentEntityId?.items?.[0]?.id}`)
+            const isViewOrEditPage = lastPath.includes("/view") || lastPath.includes("/edit");
             const liIndix =
                 parseInt(window.localStorage.getItem(`${currentUserId?.items?.[0]?.id}-${entityName}-${currentEntityId?.items?.[0]?.id}`, "1"), 10) - 1;
-                console.info("liIndix", liIndix)
             document.querySelectorAll(".mx-name-" + targetTabCtrl + " > ul").forEach((ultValue, _ulIndex, _listObj) => {
                 ultValue.querySelectorAll("li").forEach((currentValue, _currentIndex) => {
                     currentValue.addEventListener("click", () => {
@@ -29,12 +33,20 @@ const CustomTabContainer = ({ targetTabCtrl, currentUserId, currentEntityId, ent
                         );
                     });
                 });
-                const liList = ultValue.querySelectorAll("li");
-                const li = liList.item(liIndix);
-                console.info("li", li)
-                if (li != null) {
-                    li.click();
-                    li.classList.add("active");
+                if(isViewOrEditPage) {
+                    const liList = ultValue.querySelectorAll("li");
+                    const li = liList.item(liIndix);
+                    if (li != null) {
+                        li.click();
+                        li.classList.add("active");
+                    }
+                } else {
+                     const liList = ultValue.querySelectorAll("li");
+                      const li = liList.item(0);
+                    if (li != null) {
+                        li.click();
+                        li.classList.add("active");
+                    }
                 }
             });
         }
